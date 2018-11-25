43bf2819c3c2ca031dcb3eab37714173c907316c
Matthias Treydte <mt@waldheinz.de>
2018-11-25 18:30:30 +0100

    add an update-all script (hardcoded hosts for me...)

---
+++ update-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+pio run -t upload --upload-port ESP_420F8B.lan.waldheinz.de # Treppe
+pio run -t upload --upload-port esp_41ce38.lan.waldheinz.de # Wohnzimmer Sofa
+pio run -t upload --upload-port esp_41ea3f.lan.waldheinz.de # Schlafzimmer
+pio run -t upload --upload-port esp_41dfd6.lan.waldheinz.de # Küche
+pio run -t upload --upload-port esp_b949c9.lan.waldheinz.de # Spiegelschrank
+pio run -t upload --upload-port esp_b94af9.lan.waldheinz.de # Regal Schreibtisch
